19042cd36b902f06e48f754cf8db79490d0fa5fe
devforfu <developer.z@outlook.com>
2019-02-07 15:29:24 +0500

    Timer utility

--- loop/utils.py
+++ loop/utils.py
@@ -25,3 +25,40 @@ def pairs(seq):
 
 def classname(x):
     return x.__class__.__name__
+
+
+class Timer:
+    """Simple util to measure execution time.
+    Examples
+    --------
+    >>> import time
+    >>> with Timer() as timer:
+    ...     time.sleep(1)
+    >>> print(timer)
+    00:00:01
+    """
+    def __init__(self):
+        self.start = None
+        self.elapsed = None
+
+    def __enter__(self):
+        self.start = default_timer()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.elapsed = default_timer() - self.start
+
+    def __str__(self):
+        return self.verbose()
+
+    def __float__(self):
+        return self.elapsed
+
+    def verbose(self):
+        if self.elapsed is None:
+            return '<not-measured>'
+        return self.format_elapsed_time(self.elapsed)
+
+    @staticmethod
+    def format_elapsed_time(value: float):
+        return time.strftime('%H:%M:%S', time.gmtime(value))
